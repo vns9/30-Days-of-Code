6b425055a5b6d1f57e413f88823ccc8900e6c666
Venkat Narayana <nashve9@gmail.com>
2022-01-25 17:23:47 +0530

    Add files via upload

---
+++ Bottom_View_of_Binary_Tree.cpp
@@ -0,0 +1,145 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+vector <int> bottomView(Node *root);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+ // } Driver Code Ends
+//Function to return a list containing the bottom view of the given tree.
+
+class Solution {
+  public:
+    void inorder(int i, int h, Node* root, map<int,pair<int, int>>& ans){
+        if(root->left) inorder(i-1, h+1, root->left, ans);
+        if(ans.find(i)==ans.end()){
+            ans[i].first=h;
+            ans[i].second=root->data;
+        }
+        else{
+            if(h>=(ans[i].first)){
+                ans[i].first=h; ans[i].second=root->data;
+            }
+        }
+        if(root->right) inorder(i+1, h+1, root->right, ans);
+    }
+    
+    vector <int> bottomView(Node *root) {
+        // Your Code Here
+        //Your code here
+        map<int,pair<int, int>> ans;
+        vector<int> retans;
+        if(root==NULL) return retans;
+        inorder(0,0,root,ans);
+        for(auto kv: ans) retans.push_back(kv.second.second);
+        return retans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s ,ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        vector <int> res = ob.bottomView(root);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+
+  // } Driver Code Ends
